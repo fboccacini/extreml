d53fc98c240f801aab501b7eaee8e8b4d5ac3f3a
boccia <fboccacini@gmail.com>
2020-05-06 02:59:17 +0900

    first working version

--- Extreml.rb
+++ Extreml.rb
@@ -1,5 +1,6 @@
 require 'pp'
 require 'net/http'
+require 'byebug'
 
 class XmlHeader
 end
@@ -9,20 +10,72 @@ end
 
 class Type
 
-  def initialize namespace: nil, attributes: []
-    @namespace = namespace
-    @atributes = attributes
-    @types = Array.new
-  end
+  def initialize document
 
-  def add_type name, type
-
-    @types << type
-
-    define_singleton_method name.to_sym do
-      eval "return @types[#{@types.length - 1}]"
+    @name = document[:name]
+    @namespace = document[:namespace]
+    @attributes = document[:attributes]
+    @content = document[:content]
+    unless @content.nil?
+      @content.each do |v|
+        add_type v[:name], v
+      end
     end
 
+  end
+
+  def name
+    @name
+  end
+
+  def attributes
+    @attributes
+  end
+
+  def namespace
+    @namespace
+  end
+
+  def method_names
+    return self.methods - Object.methods
+  end
+
+  def __types
+    return self.method_names - [:add_type, :namespace, :attributes, :__types, :tree, :method_names]
+  end
+
+  def add_type name, content
+
+    if self.__types.any? name.to_sym
+      array = self.send name.to_sym
+      define_singleton_method name.to_sym do
+        return [array].flatten + [(content.class == Hash ? (Type.new content) : content)]
+      end
+    else
+      define_singleton_method name.to_sym do
+        if content.class == Hash
+          return Type.new content
+        else
+          return content
+        end
+      end
+    end
+
+
+  end
+
+  def tree level = 0
+
+    puts "#{'   ' * level}|#{'__'}#{self.namespace}:#{self.name} #{self.__types.inspect} #{self.attributes}"
+    level += 1
+    # pp @content
+    # gets
+    self.__types.each do |m|
+      next_type = self.send(m)
+      [next_type].flatten.each do |nt|
+        nt.tree level unless next_type.nil?
+      end
+    end
   end
 end
 
@@ -70,7 +123,15 @@ class Extreml
     # Read document
     doc = xml.match /(?:\<\?xml .*?(?: ?\?\>))?[\t\n\r\f ]*(.*)/m
     @document = unpack doc[1]
+    define_singleton_method :document do
+      return Type.new({name: 'document', content: @document})
+    end
 
+    # unpack(doc[1]).each do |pack|
+    #
+    #   @document = Type.new namespace: pack.namespace, attributes: pack.attributes, name: pack.name
+    #   @document.add_type pack, method_name: 'definitions'
+    # end
       # xsd_address = @wsdl_definitions[]
       # Net::HTTP.start("somedomain.net") do |http|
       #   resp = http.get("/flv/sample/sample.flv")
@@ -78,6 +139,10 @@ class Extreml
       #       file.write(resp.body)
       #   end
       # end
+  end
+
+  def tree
+    self.document.tree
   end
 
   def document
@@ -103,50 +168,18 @@ class Extreml
           attributes = Array.new
           a = parts[:attributes].scan /[\t\n\r\f ]*(?:(?<namespace>[^:\n\r\f\t]*):)?(?<property>[^=\n\r\f\t ]*)[\t ]*=[\t ]*"(?<value>[^"]*)"[\t\n\r\f]*/m
           a.each do |p|
-
             attributes << {
               :namespace => p[0],
               :property => p[1],
               :value => p[2]
             }
           end
-          # puts "#{parts[:name]} - #{defined?(parts[:name].to_s)}"
-          # puts "#{parts[:name]} - #{defined?("blabla")}"
-          # name = parts[:name][0].upcase + parts[:name][1..-1]
-          # puts name
-          # unless defined?(name) == 'constant' && name.class == Class
-          #   classname = Object.const_set(name, Class.new do
-          #
-          #
-          #   end)
-          #
-          #   # class << parts[:name]; Type do
-          #   #
-          #   # end
-          # end
-          # puts "#{defined?(name)} && #{name.class == Class}"
-          if defined? self.parts[:name]
 
-          else
-            pack = Type.new ({
-              :namespace => parts[:namespace],
-              :attributes => attributes
-            })
-
-            pack.add_type tags[:name], nil
-            return pack
-          end
-          # pack = Hash[
-          #   :name => parts[:name],
-            # :namespace => parts[:namespace],
-            # :attributes => attributes
-          #
-          # ]
-          # pp pack
-          # puts pack.inspect
-          # puts defined? self.parts[:name]
-          # puts self.parts[:name].methods
-          # self.call(parts[:name])
+          pack = Hash[
+            :name => parts[:name],
+            :namespace => parts[:namespace],
+            :attributes => attributes
+          ]
           return [unpack(parts[:prerest].strip), pack, unpack(parts[:rest].strip)].reject{ |p| p == ""}
         end
       else
@@ -174,20 +207,127 @@ class Extreml
         end
         pack[:content] += [unpack(tags[:content].strip)].flatten
       end
-      pack = Type.new ({
-        :namespace => parts[:namespace],
-        :attributes => attributes
-      })
-
-      pack.add_type tags[:name], unpack(tags[:content].strip)
-      return pack
-      # return [unpack(tags[:prerest].strip), pack, unpack(tags[:rest].strip)].reject{ |p| p == ""}
+      return [unpack(tags[:prerest].strip), pack, unpack(tags[:rest].strip)].reject{ |p| p == ""}
 
     end
   rescue Exception => e
     puts e.message
     puts e.backtrace.join("\n")
   end
+
+    # pack = nil
+    # string = string.gsub(/\<\!--[^>]*--\>/,'').strip
+    # begin
+    #   string = string.strip
+    #   tags = string.match /(?<prerest>.*?)?\<(?:(?<namespace>\w*):)?(?<name>[^\/]\w*)(?:[\t]*(?<attributes>[^>]*)?)\>(?<content>.*?)[\t\n\r\f]*\<\/\k<namespace>?:?\k<name>\>(?<rest>.*)/m
+    #
+    #   if tags.nil?
+    #     parts = string.match /(?<prerest>.*?)?\<(?:(?<namespace>\w*):)?(?<name>[^\/]\w*)(?:[\t]*(?<attributes>[^>]*)?)[\t\n\r\f]*\/\>(?<rest>.*)/m
+    #
+    #     if parts.nil?
+    #       return string
+    #     else
+    #       attributes = Array.new
+    #       a = parts[:attributes].scan /[\t\n\r\f ]*(?:(?<namespace>[^:\n\r\f\t]*):)?(?<property>[^=\n\r\f\t ]*)[\t ]*=[\t ]*"(?<value>[^"]*)"[\t\n\r\f]*/m
+    #       a.each do |p|
+    #         attributes << {
+    #           :namespace => p[0],
+    #           :property => p[1],
+    #           :value => p[2]
+    #         }
+    #       end
+    #
+    #       pack = Hash[
+    #         :name => parts[:name],
+    #         :namespace => parts[:namespace],
+    #         :attributes => attributes
+    #       ]
+    #
+    #       if defined? self.parts[:name]
+    #
+    #       else
+    #
+    #
+    #       end
+    #
+    #       type = Type.new ({
+    #         :name => parts[:name],
+    #         :namespace => parts[:namespace],
+    #         :attributes => attributes
+    #       })
+    #       # pack = Hash[
+    #       #   :name => parts[:name],
+    #         # :namespace => parts[:namespace],
+    #         # :attributes => attributes
+    #       #
+    #       # ]
+    #       # pp pack
+    #       # puts pack.inspect
+    #       # puts defined? self.parts[:name]
+    #       # puts self.parts[:name].methods
+    #       # self.call(parts[:name])
+    #       return [unpack(parts[:prerest].strip), type, unpack(parts[:rest].strip)].reject{ |p| p == ""}
+    #     end
+    #   else
+    #
+    #     if tags[:attributes].strip! == ""
+    #       attributes = nil
+    #     else
+    #       attributes = Array.new
+    #       a = tags[:attributes].scan /[\t\n\r\f ]*(?:(?<namespace>[^:\n\r\f\t]*):)?(?<property>[^=\n\r\f\t ]*)[\t ]*=[\t ]*"(?<value>[^"]*)"[\t\n\r\f]*/m
+    #       a.each do |p|
+    #         attributes << {
+    #           :namespace => p[0],
+    #           :property => p[1],
+    #           :value => p[2]
+    #         }
+    #       end
+    #     end
+    #     type = Type.new ({
+    #       :name => tags[:name],
+    #       :namespace => tags[:namespace],
+    #       :attributes => attributes
+    #     })
+    #     pp tags[:content].strip
+    #     box = unpack(tags[:content].strip)
+    #     pp box
+    #     box.flatten.each do |t|
+    #       puts "#{type.name} -> #{t.name}"
+    #       type.add_type t
+    #     end
+    #     gets
+    #     # pack.each do |p|
+    #     #   next if p.nil?
+    #     #   # p.each do |pp|
+    #     #     if p.class == Type
+    #     #       pack.add_type p
+    #     #     else
+    #     #       pp p
+    #     #       pack.add_type p, method_name: tags[:name]
+    #     #     end
+    #     #   # end
+    #     # end
+    #     # if pack.nil?
+    #     #   pack = Hash[
+    #     #     :name => tags[:name].strip,
+    #     #     :namespace => tags[:namespace].nil? ? nil : tags[:namespace].strip,
+    #     #     :attributes => attributes,
+    #     #     :content => Array.new
+    #     #   ]
+    #     # end
+    #     #
+    #     # pack[:content] += [unpack(tags[:content].strip)].flatten
+    #   end
+    #
+    #
+    #   # return pack
+    #   return [unpack(tags[:prerest].strip), type, unpack(tags[:rest].strip)].reject{ |p| p == ""}
+
+  #   end
+  # rescue Exception => e
+  #   puts e.message
+  #   puts e.backtrace.join("\n")
+  # end
 
 end
 
@@ -205,8 +345,10 @@ test = Extreml.new(ric)
 #
 #
 # end
-puts test.xml.class
-puts test.xml.version
-puts NewClass.test
-puts Otherclass.method1
+# puts test.xml.class
+# puts test.xml.version
+pp test.document.definitions.tree
+# pp test.document.definitions.method_names
+# pp test.document.definitions.message.part.method_names
+# test.document.definitions.portType.tree
 # puts test.version
