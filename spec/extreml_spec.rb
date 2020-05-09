f21767be0ad32103aeab6ff114ce47145959353f
boccia <fboccacini@gmail.com>
2020-05-09 21:13:26 +0900

    work in progress

---
+++ spec/extreml_spec.rb
@@ -0,0 +1,11 @@
+describe Extreml do
+   context “When testing the HelloWorld class” do 
+
+      it "should say 'Hello World' when we call the say_hello method" do
+         hw = HelloWorld.new
+         message = hw.say_hello
+         expect(message).to eq "Hello World!"
+      end
+
+   end
+end
